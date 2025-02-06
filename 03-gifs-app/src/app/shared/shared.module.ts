9378d11446e844479a64dec2426a7b101adbe8f6
Doplax <doplax@gmail.com>
2025-02-06 14:02:59 +0100

    feat: Diseño de Gifs Components

--- 03-gifs-app/src/app/shared/shared.module.ts
+++ 03-gifs-app/src/app/shared/shared.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { HomePageComponent } from './pages/home/home-page.component';
+import { SearchBoxComponent } from './components/searchBox/searchBox.component';
+
 
 
 
 @NgModule({
   declarations: [
-    SidebarComponent
+    SidebarComponent,
+    HomePageComponent,
+    SearchBoxComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     SidebarComponent,
+    HomePageComponent
   ]
 })
 export class SharedModule { }
