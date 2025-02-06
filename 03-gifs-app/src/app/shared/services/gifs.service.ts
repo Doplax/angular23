e729eb8934733067dbe10aa0be1879213be49e18
Doplax <doplax@gmail.com>
2025-02-06 16:37:22 +0100

    feat: Validaciones al servicio

--- 03-gifs-app/src/app/shared/services/gifs.service.ts
+++ 03-gifs-app/src/app/shared/services/gifs.service.ts
@@ -13,9 +13,19 @@ export class GifsService {
     return [...this._tagsHistory];
   }
 
-  searchTag(tag: string): void{
-    this._tagsHistory.unshift(tag);
-    console.log(this.tagsHistory);
+  private organizeHistory(tag: string) {
+    tag = tag.toLocaleLowerCase();
 
+    if (this._tagsHistory.includes(tag)) {
+      this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
+    }
+
+    this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.splice(0, 10);
+  }
+
+  searchTag(tag: string): void{
+    if (tag.length === 0) { return;}
+    this.organizeHistory(tag);
   }
 }
