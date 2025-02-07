f0d3cf736a33846632febc18ca2928405a66cdf5
Doplax <doplax@gmail.com>
2025-02-07 10:20:22 +0100

    feat: Mostrar los Gifs en pantalla

--- 03-gifs-app/src/app/shared/components/cardList/cardList.component.ts
+++ 03-gifs-app/src/app/shared/components/cardList/cardList.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Gif } from 'src/app/gifs/interfaces/gifs.interfaces';
 
 @Component({
   selector: 'app-card-list',
   templateUrl: './cardList.component.html',
   styleUrls: ['./cardList.component.css'],
 })
-export class CardListComponent { }
+export class CardListComponent {
+  @Input() gifs: Gif[] = [];
+}
--- 03-gifs-app/src/app/shared/pages/home/home-page.component.ts
+++ 03-gifs-app/src/app/shared/pages/home/home-page.component.ts
@@ -1,7 +1,18 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+import { Gif } from 'src/app/gifs/interfaces/gifs.interfaces';
 
 @Component({
   selector: 'gifs-home-page',
   templateUrl: './home-page.component.html',
 })
-export class HomePageComponent { }
+export class HomePageComponent {
+
+
+  constructor( private gifsService: GifsService) {}
+
+  get gifs(): Gif[] {
+    return this.gifsService.gifsList;
+  }
+
+}
