a8cb958afe865548244b22d8efaf036175ec93ba
Doplax <doplax@gmail.com>
2025-02-07 17:40:17 +0100

    feat: Router Module y Páginas - SPA

--- 04-countryApp/src/app/app.module.ts
+++ 04-countryApp/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-  ],
+
+],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ 04-countryApp/src/app/shared/pages/aboutPage/aboutPage.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-about-page',
+  standalone: false,
+  templateUrl: './aboutPage.component.html',
+})
+export class AboutPageComponent { }
