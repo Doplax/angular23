80587bb360d142333988f5913ea8cde2d5bc0b89
Doplax <doplax@gmail.com>
2025-02-11 11:20:55 +0100

    feat: CatchError - Error handling in observables

--- 04-countryApp/src/app/countries/services/countries.service.ts
+++ 04-countryApp/src/app/countries/services/countries.service.ts
@@ -1,17 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, count, map, Observable, of, tap } from 'rxjs';
 import { Country } from '../interfaces/country';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class CountriesService {
-
   private url = 'https://restcountries.com/v3.1';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
-
-  searchByCapital(term: string): Observable<Country[]>{
-    return this.httpClient.get<Country[]>(`${this.url}/capital/${term}`);
+  searchByCapital(term: string): Observable<Country[]> {
+    const url = `${this.url}/capital/${term}`;
+    return this.httpClient
+      .get<Country[]>(url)
+      .pipe(catchError(() => of([])));
   }
 }
