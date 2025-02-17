e56ea715fed1152f7f79d0fc3bece10eb2b54a83
Doplax <doplax@gmail.com>
2025-02-17 16:35:42 +0100

    feat: PrimeNG - MenuBar

--- 05-pipesApp/src/app/shared/components/menu/menu.component.ts
+++ 05-pipesApp/src/app/shared/components/menu/menu.component.ts
@@ -4,16 +4,40 @@ import { MenuItem } from 'primeng/api';
 @Component({
   selector: 'shared-menu',
   templateUrl: './menu.component.html',
-  styles: [''],})
+  styles: [''],
+})
 export class MenuComponent {
   menuItems: MenuItem[] = [];
 
-
   ngOnInit() {
     this.menuItems = [
-      {label: 'New', icon: 'pi pi-fw pi-plus'},
-      {label: 'Open', icon: 'pi pi-fw pi-download'},
-      {label: 'Undo', icon: 'pi pi-fw pi-refresh'},
-    ]
+      {
+        label: 'Pipes de Angular',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Textos y Fechas',
+            icon: 'pi pi-align-left',
+            routerLink: '/',
+          },
+          { label: 'Números', icon: 'pi pi-dollar', routerLink: 'numeros' },
+          {
+            label: 'No comunes',
+            icon: 'pi pi-globe',
+            routerLink: 'no-comunes',
+          },
+        ],
+      },
+      {
+        label: 'Pipes personalizados',
+        icon: 'pi pi-cog',
+        items: [
+          {
+            label: 'Otro elemento',
+            icon: 'pi pi-cog',
+          },
+        ],
+      },
+    ];
   }
 }
