dfe2c4ebdc1fa6847c6013d20878686581644a97
Doplax <doplax@gmail.com>
2025-02-21 10:31:02 +0100

    feat: sortBy - Custom Pipe

--- 05-pipesApp/src/app/products/products.module.ts
+++ 05-pipesApp/src/app/products/products.module.ts
@@ -8,7 +8,8 @@ import { OrderComponent } from './pages/order/order.component';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 
 import { ToggleCasePipe } from './pipes/toggle-case.pipe';
-
+import { CanFly } from './pipes/can-fly.pipe';
+import { SortByPipe } from './pipes/sort-by.pipe';
 
 @NgModule({
   declarations: [
@@ -16,12 +17,10 @@ import { ToggleCasePipe } from './pipes/toggle-case.pipe';
     NumbersPageComponent,
     UncommonPageComponent,
     OrderComponent,
-    ToggleCasePipe
+    ToggleCasePipe,
+    CanFly,
+    SortByPipe
   ],
-  imports: [
-    CommonModule,
-    PrimeNgModule,
-    ProductsRoutingModule,
-  ]
+  imports: [CommonModule, PrimeNgModule, ProductsRoutingModule],
 })
-export class ProductsModule { }
+export class ProductsModule {}
