134d043ba6cab9b20c27cc21e22470b3b35d6d58
Doplax <doplax@gmail.com>
2025-02-27 13:05:34 +0100

    feat: Paths of missing routes

--- 06-heroesApp/src/app/heroes/heroes.module.ts
+++ 06-heroesApp/src/app/heroes/heroes.module.ts
@@ -4,12 +4,18 @@ import { CommonModule } from '@angular/common';
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 
 
 @NgModule({
   declarations: [
     LayoutPageComponent,
-    NewPageComponent
+    NewPageComponent,
+    SearchPageComponent,
+    ListPageComponent,
+    HeroPageComponent
   ],
   imports: [
     CommonModule,
