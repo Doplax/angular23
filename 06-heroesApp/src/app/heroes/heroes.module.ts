7cc7c7a05492144bf2287ef51a77bc3ac8b7829c
Doplax <doplax@gmail.com>
2025-02-27 12:39:49 +0100

    feat: Child Route and LazyLoad

--- 06-heroesApp/src/app/heroes/heroes.module.ts
+++ 06-heroesApp/src/app/heroes/heroes.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutPageComponent,
+    NewPageComponent
+  ],
   imports: [
     CommonModule,
     HeroesRoutingModule
