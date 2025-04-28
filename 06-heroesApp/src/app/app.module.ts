ea025e2d6879a7c839e268b93ece8005d69607c9
Doplax <doplax@gmail.com>
2025-04-28 11:01:08 +0200

    feat: install Angular Material

--- 06-heroesApp/src/app/app.module.ts
+++ 06-heroesApp/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +12,10 @@ import { SharedModule } from './shared/shared.module';
     AppComponent
   ],
   imports: [
-    AppRoutingModule,
     BrowserModule,
-    SharedModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
