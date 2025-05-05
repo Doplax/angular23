3f304ef74c5de89032599058cdc913bab229ad38
Doplax <doplax@gmail.com>
2025-05-05 11:00:21 +0200

    feat: Login and Auth Layout Design

--- 06-heroesApp/src/app/material/material.module.ts
+++ 06-heroesApp/src/app/material/material.module.ts
@@ -1,6 +1,15 @@
 import { NgModule } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  exports: [],
+  exports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
 export class MaterialModule {}
