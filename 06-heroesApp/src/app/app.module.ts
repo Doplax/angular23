0f0be78e3b0158a8fc141413b26681bb09c0e5a3
Doplax <doplax@gmail.com>
2025-05-05 12:57:14 +0200

    feat: HeroesService - Traer información de los héroes

--- 06-heroesApp/src/app/app.module.ts
+++ 06-heroesApp/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,7 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     SharedModule,
   ],
-  providers: [],
+  providers: [provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- 06-heroesApp/src/app/heroes/heroes.module.ts
+++ 06-heroesApp/src/app/heroes/heroes.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http'; // ¡Importa HttpClientModule aquí!
 
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
@@ -8,7 +9,6 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { ListPageComponent } from './pages/list-page/list-page.component';
 import { HeroPageComponent } from './pages/hero-page/hero-page.component';
 import { MaterialModule } from '../material/material.module';
-
 
 @NgModule({
   declarations: [
@@ -21,8 +21,9 @@ import { MaterialModule } from '../material/material.module';
   imports: [
     CommonModule,
     HeroesRoutingModule,
-    MaterialModule
-
-  ]
+    MaterialModule,
+    HttpClientModule // ¡Asegúrate de que HttpClientModule esté aquí!
+  ],
+  providers: [] // No necesitas provideHttpClient() aquí para módulos tradicionales
 })
 export class HeroesModule { }
