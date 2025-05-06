7a5330c76841bf2bc63436ed8ce04a1b416201fa
Doplax <doplax@gmail.com>
2025-05-06 14:18:15 +0200

    feat: Hero Page

--- 06-heroesApp/src/app/heroes/pages/hero-page/hero-page.component.ts
+++ 06-heroesApp/src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,35 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { HeroesService } from '../../services/heroes.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from 'rxjs';
+
+import { HeroesService } from '../../services/heroes.service';
 import { Hero } from '../../interfaces/hero.interfaces';
 
 @Component({
   selector: 'app-hero-page',
-  standalone: false,
   templateUrl: './hero-page.component.html',
-  styleUrl: './hero-page.component.css'
+  styles: [
+  ]
 })
-export class HeroPageComponent implements OnInit{
+export class HeroPageComponent implements OnInit {
 
   public hero?: Hero;
+
   constructor(
-    private HeroesService: HeroesService,
+    private heroesService: HeroesService,
     private activatedRoute: ActivatedRoute,
-    private router: Router
-  ) { }
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {
     this.activatedRoute.params
-    .pipe(
-      switchMap(({id}) => this.HeroesService.getHeroById(id)) // Cambia el id por el id que viene de la ruta
+      .pipe(
+        switchMap( ({ id }) => this.heroesService.getHeroById( id )),
+      )
+      .subscribe( hero => {
 
-    ).subscribe(hero => {
-      if (!hero) return this.router.navigate(['/heroes/list']); // Si no hay héroe, redirige a la lista de héroes
-      this.hero = hero; // Asigna el héroe a la variable hero
-      console.log({hero})
-      return; // Devuelve el héroe
-    });
+        if ( !hero ) return this.router.navigate([ '/heroes/list' ]);
+
+        this.hero = hero;
+        return;
+      })
+  }
+
+  goBack():void {
+    this.router.navigateByUrl('heroes/list')
   }
 
 }
--- 06-heroesApp/src/app/material/material.module.ts
+++ 06-heroesApp/src/app/material/material.module.ts
@@ -6,6 +6,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatGridListModule } from '@angular/material/grid-list';
+
 
 @NgModule({
   exports: [
@@ -15,7 +18,9 @@ import { MatListModule } from '@angular/material/list';
     MatIconModule,
     MatSidenavModule,
     MatToolbarModule,
-    MatListModule
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatGridListModule
   ],
 })
 export class MaterialModule {}
