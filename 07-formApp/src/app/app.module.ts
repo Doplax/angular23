b8cc2c379f2f0ebb12e2dbfe7bfcafdc2fb5f93f
Doplax <doplax@gmail.com>
2025-05-21 14:14:40 +0200

    feat: creates formApp

---
+++ 07-formApp/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { provideHttpClient } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+  ],
+  providers: [provideHttpClient()],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 07-formApp/src/app/auth/pages/registerPage/registerPage.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-page',
+  templateUrl: './registerPage.component.html',
+  styleUrl: './registerPage.component.css',
+  standalone: false,
+})
+export class RegisterPageComponent { }
