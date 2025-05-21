839089d1df7db62ec67ddf79a096b75a028147be
Doplax <doplax@gmail.com>
2025-05-21 15:33:23 +0200

    feat: routes of our application

--- 07-formApp/src/app/auth/auth.module.ts
+++ 07-formApp/src/app/auth/auth.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-
+import { RegisterPageComponent } from "./pages/registerPage/registerPage.component";
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RegisterPageComponent,
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
--- 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  imports: [],
   templateUrl: './basic-page.component.html',
-  styles: ``
+  styles: ``,
+  standalone: false,
 })
 export class BasicPageComponent {
 
