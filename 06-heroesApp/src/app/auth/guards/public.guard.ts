d28a9238af772ed31cf11af14809f788f2c674f6
Doplax <doplax@gmail.com>
2025-05-21 10:37:52 +0200

    feat: PublicGuard - Task

--- 06-heroesApp/src/app/auth/guards/public.guard.ts
+++ 06-heroesApp/src/app/auth/guards/public.guard.ts
@@ -1,10 +1,10 @@
-``import { Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivate, CanMatch, GuardResult, MaybeAsync, Route, RouterStateSnapshot, UrlSegment, Router } from '@angular/router';
-import { Observable, tap } from "rxjs";
+import { map, Observable, tap } from "rxjs";
 import { AuthService } from '../services/auth.service';
 
 @Injectable({ providedIn: 'root' })
-export class AuthGuard implements CanMatch, CanActivate{
+export class PublicGuard implements CanMatch, CanActivate{
   constructor(
     private authService:AuthService,
     private router: Router
@@ -14,22 +14,24 @@ export class AuthGuard implements CanMatch, CanActivate{
     return this.authService.checkAuthentication()
       .pipe(
         tap( isAuthenticated => {
-          if (!isAuthenticated) {
-            this.router.navigate(['./auth/login']);
+          if (isAuthenticated) {
+            this.router.navigate(['./']);
           }
- ``       })
+        }),
+        map( isAuthenticated => !isAuthenticated), // <-- devuelve true si no hay un usuario, false si hay
       )
   }
 
   canMatch(route: Route, segments: UrlSegment[]): boolean | Observable<boolean> {
-    console.log('canMatch');
-    console.log({ route, segments });
+    //console.log('canMatch');
+    //console.log({ route, segments });
     return true;
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> {
     console.log('canActivate');
     console.log({route, state});
-    return true;
+    return this.ceckAuthenticationStatus();
+    ;
   }
 }
