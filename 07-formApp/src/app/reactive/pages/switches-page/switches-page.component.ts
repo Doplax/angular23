e39092460dfd09c2094db33fcc1e7a7dff3ffeb4
Doplax <doplax@gmail.com>
2025-05-21 15:08:39 +0200

    feat: HTML structure of the pages

---
+++ 07-formApp/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './switches-page.component.html',
+  styles: ``
+})
+export class SwitchesPageComponent {
+
+}
