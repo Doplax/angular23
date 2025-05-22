4510e127cf8150ee3619492cfdabb2ef5b6507d0
Doplax <doplax@gmail.com>
2025-05-22 11:33:08 +0200

    feat: first steps in reactive forms

--- 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ 07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class BasicPageComponent {
+  //public myForm: FormGroup = new FormGroup({
+  //  name: new FormGroup(''),
+  //  price: new FormGroup(0),
+  //  inStorage: new FormGroup(0),
+  //});
 
+  public myForm: FormGroup;
+
+  constructor(private fb:FormBuilder) {
+    this.myForm = this.fb.group({
+      name: [''],
+      price: [0],
+      inStorage: [0],
+    });
+  }
+
+  onSave():void {
+    console.log(this.myForm.value);
+  }
 }
