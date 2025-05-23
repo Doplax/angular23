ebcba8ffa227bea62904a5113342496e5b9aa91e
Doplax <doplax@gmail.com>
2025-05-23 10:42:05 +0200

    feat: dynamic forms

--- 07-formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ 07-formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -7,4 +8,39 @@ import { Component } from '@angular/core';
 })
 export class DynamicPageComponent {
 
+  public myForm: FormGroup;
+  public myFormGames: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+  ){
+    this.myForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      favoriteGames: this.fb.array([])
+    });
+
+    this.myFormGames = this.fb.group({
+      favoriteGames: this.fb.array([
+        ['Metal Gear Solid', [Validators.required]],
+        ['Final Fantasy VII', [Validators.required]],
+        ['The Legend of Zelda: Ocarina of Time', [Validators.required]],
+        ['Super Mario 64', [Validators.required]],
+      ])
+    });
+  }
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+
+  onSumbit(): void {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+    this.myForm.reset();
+  }
 }
