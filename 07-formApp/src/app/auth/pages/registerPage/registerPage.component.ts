f0875b514eff3a624389b078f1e7ef387c9c4452
Doplax <doplax@gmail.com>
2025-06-22 12:55:42 +0200

    feat: Validator Functions

--- 07-formApp/src/app/auth/pages/registerPage/registerPage.component.ts
+++ 07-formApp/src/app/auth/pages/registerPage/registerPage.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
@@ -6,4 +7,33 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './registerPage.component.css',
   standalone: false,
 })
-export class RegisterPageComponent { }
+export class RegisterPageComponent {
+
+  public myForm:FormGroup
+
+
+  constructor(
+    private fb: FormBuilder
+  ){
+    this.myForm = this.fb.group({
+      name: ['', [ Validators.required, Validators.pattern( this.validatorsService.firstNameAndLastnamePattern )  ]],
+      // email: ['', [ Validators.required, Validators.pattern( this.validatorsService.emailPattern )], [ new EmailValidator() ]],
+      email: ['', [ Validators.required, Validators.pattern( this.validatorsService.emailPattern )], [ this.emailValidator ]],
+      username: ['', [ Validators.required, this.validatorsService.cantBeStrider ]],
+      password: ['', [ Validators.required, Validators.minLength(6) ]],
+      password2: ['', [ Validators.required ]],
+    }, {
+      validators: [
+        this.validatorsService.isFieldOneEqualFieldTwo('password','password2')
+      ]
+    });
+  }
+
+  isValidField(field: string): boolean {
+
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
+ }
