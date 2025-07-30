866a980683b3384480061bf647255b1d6086804d
Doplax <doplax@gmail.com>
2025-07-30 12:50:15 +0200

    feat: Email Validations and Patterns

--- 07-formApp/src/app/auth/pages/registerPage/registerPage.component.ts
+++ 07-formApp/src/app/auth/pages/registerPage/registerPage.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as customValidators from '../../../shared/validators/validators';
 
 @Component({
   selector: 'app-register-page',
@@ -16,22 +17,21 @@ export class RegisterPageComponent {
     private fb: FormBuilder
   ){
     this.myForm = this.fb.group({
-      name: ['', [ Validators.required, Validators.pattern( this.validatorsService.firstNameAndLastnamePattern )  ]],
-      // email: ['', [ Validators.required, Validators.pattern( this.validatorsService.emailPattern )], [ new EmailValidator() ]],
-      email: ['', [ Validators.required, Validators.pattern( this.validatorsService.emailPattern )], [ this.emailValidator ]],
-      username: ['', [ Validators.required, this.validatorsService.cantBeStrider ]],
+      name: ['', [ Validators.required, Validators.pattern( customValidators.firstNameAndLastnamePattern )  ]],
+      email: ['', [ Validators.required, Validators.pattern( customValidators.emailPattern )]],
+      username: ['', [ Validators.required, customValidators.cantBeStrider ]],
       password: ['', [ Validators.required, Validators.minLength(6) ]],
       password2: ['', [ Validators.required ]],
     }, {
-      validators: [
-        this.validatorsService.isFieldOneEqualFieldTwo('password','password2')
-      ]
+      //validators: [
+      //  this.validatorsService.isFieldOneEqualFieldTwo('password','password2')
+      //]
     });
   }
 
-  isValidField(field: string): boolean {
+  //isValidField(field: string): boolean {
 
-  }
+  //}
 
   onSubmit() {
     this.myForm.markAllAsTouched();
