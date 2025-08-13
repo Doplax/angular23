524b8096b3a51013f740610377eb566dd32fadce
Doplax <doplax@gmail.com>
2025-08-13 17:01:20 +0200

    feat: Develop the email validator

--- 07-formApp/src/app/shared/validators/email-validator.service.ts
+++ 07-formApp/src/app/shared/validators/email-validator.service.ts
@@ -13,7 +13,23 @@ export class EmailValidator implements AsyncValidator {
   validate( control: AbstractControl ): Observable<ValidationErrors | null> {
     const email = control.value;
 
-    return of({ emailTaken: true }).pipe(delay(3000));// Simula una llamada a un servicio que verifica si el email ya está en uso
+    const httpCallObservable = new Observable<ValidationErrors | null>((subscriber) => {
+      console.log({email})
+
+      if( email === "doplax@gmail.com") {
+        subscriber.next({ emailTaken: true });
+        subscriber.complete();
+      }
+
+      subscriber.next(null);
+      subscriber.complete();
+    }).pipe(
+      delay(3000) // Simula un retraso de 3 segundos para la llamada HTTP
+    );
+
+    return httpCallObservable;
+
+    //return of({ emailTaken: true }).pipe(delay(3000));// Simula una llamada a un servicio que verifica si el email ya está en uso
   }
 
   //registerOnValidatorChange?(fn: () => void): void {
