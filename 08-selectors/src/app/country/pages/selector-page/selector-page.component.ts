a613ad2640a3b450a077a33ac2e082494854d3a1
Doplax <doplax@gmail.com>
2025-08-18 15:44:47 +0200

    feat: scaffolding

---
+++ 08-selectors/src/app/country/pages/selector-page/selector-page.component.ts
@@ -0,0 +1,13 @@
+import { Component, type OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-selector-page',
+  standalone: false,
+  templateUrl: './selector-page.component.html',
+  styleUrls: ['./selector-page.component.css'],
+})
+export class SelectorPageComponent implements OnInit {
+
+  ngOnInit(): void { }
+
+}
