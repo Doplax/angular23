5c6a395d5e4a97137a6856cf72aec40eb3adfbcf
Doplax <doplax@gmail.com>
2025-08-19 15:20:06 +0200

    feat: convert contries and display them on screen

--- 08-selectors/src/app/country/services/countries.service.ts
+++ 08-selectors/src/app/country/services/countries.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Country, Region, SmallCountry } from '../interfaces/country.interfaces';
-import { Observable, of, tap } from 'rxjs';
+import { count, map, Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -26,8 +26,13 @@ export class CountriesService {
 
     const url: string = `${this.baseUrl}region/${region}?fields=flags,name,cca3`;
 
-    return this.http.get<SmallCountry[]>(url).pipe(
-      tap(response => console.log({response}))
+    return this.http.get<Country[]>(url)
+    .pipe(
+      map(countries => countries.map(country => ({
+        name: country.name.common,
+        cca3: country.cca3,
+        borders: country.borders ?? [],
+      }))),
     );
   }
 
