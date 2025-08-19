8450ead10213baf07d0bcc260f92d4b62376bd72
Doplax <doplax@gmail.com>
2025-08-19 16:36:49 +0200

    feat: third nested selector - frontiers

--- 08-selectors/src/app/country/pages/selector-page/selector-page.component.ts
+++ 08-selectors/src/app/country/pages/selector-page/selector-page.component.ts
@@ -2,7 +2,7 @@ import { Component, type OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region, SmallCountry } from '../../interfaces/country.interfaces';
-import { switchMap, tap } from 'rxjs';
+import { filter, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
@@ -13,7 +13,7 @@ import { switchMap, tap } from 'rxjs';
 export class SelectorPageComponent implements OnInit {
 
   public countriesByRegion: SmallCountry[] = [];
-
+  public borders: string[] = [];
   public myForm!: FormGroup;
 
   constructor(
@@ -23,7 +23,7 @@ export class SelectorPageComponent implements OnInit {
     this.myForm = this.fb.group({
       region: ['', Validators.required],
       country: ['', Validators.required],
-      city: ['', Validators.required],
+      border: ['', Validators.required],
     });
   }
 
@@ -39,10 +39,24 @@ export class SelectorPageComponent implements OnInit {
     this.myForm.get('region')!.valueChanges
       .pipe(
         tap(() => this.myForm.get('country')!.setValue('')),
+        tap(() => this.borders = []),
         switchMap(region => this.countriesService.getCountriesByRegion(region))
       )
       .subscribe(countries => {
         this.countriesByRegion = countries
       });
+  }
+
+  onCountryChanged(): void {
+    this.myForm.get('country')!.valueChanges
+    .pipe(
+      tap(() => this.myForm.get('country')!.setValue('')),
+      filter((alphaCode:string) => alphaCode.length > 0),
+      switchMap(alphaCode => this.countriesService.getCountryByAlphaCode(alphaCode))
+    )
+    .subscribe(country => {
+      console.log({borders: country.borders});
+      this.borders = country.borders;
+    });
   }
 }
--- 08-selectors/src/app/country/services/countries.service.ts
+++ 08-selectors/src/app/country/services/countries.service.ts
@@ -36,4 +36,16 @@ export class CountriesService {
     );
   }
 
+  getCountryByAlphaCode(alphaCode: string): Observable<SmallCountry> {
+    const url: string = `${this.baseUrl}alpha/${alphaCode}?fields=flags,name,cca3,borders`;
+    return this.http.get<Country>(url)
+    .pipe(
+      map(country => ({
+        name: country.name.common,
+        cca3: country.cca3,
+        borders: country.borders ?? [],
+      }))
+    )
+  }
+
 }
