61cfedaa8d070e8853cc4a4f64067bed2a6c7b8e
Doplax <doplax@gmail.com>
2025-08-19 11:37:18 +0200

    feat: first selector

--- 08-selectors/src/app/country/pages/selector-page/selector-page.component.ts
+++ 08-selectors/src/app/country/pages/selector-page/selector-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, type OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-selector-page',
@@ -8,6 +9,18 @@ import { Component, type OnInit } from '@angular/core';
 })
 export class SelectorPageComponent implements OnInit {
 
-  ngOnInit(): void { }
+  public myForm!: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.myForm = this.fb.group({
+      region: ['', Validators.required],
+      country: ['', Validators.required],
+      city: ['', Validators.required],
+    });
+
+  }
+
+  ngOnInit(): void { }
 }
