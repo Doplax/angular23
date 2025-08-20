d4f559364d4f77a7bdb1ae3ece871f58f866e4d9
Doplax <doplax@gmail.com>
2025-08-20 15:23:24 +0200

    feat: Implements all Lifecycle Hooks

---
+++ 09-lifecycle/src/app/products/components/price/price.component.ts
@@ -0,0 +1,13 @@
+import { Component, type OnInit } from '@angular/core';
+
+@Component({
+  selector: 'products-price',
+  standalone: false,
+  templateUrl: './price.component.html',
+  styleUrls: ['./price.component.css'],
+})
+export class PriceComponent implements OnInit {
+
+  ngOnInit(): void { }
+
+}
