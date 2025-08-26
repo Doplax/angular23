d26db375a466ecc2489498f80c1b43c3413a77a7
Doplax <doplax@gmail.com>
2025-08-26 21:02:05 +0200

    feat: Control the zoom level

--- 10-MappApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
+++ 10-MappApp/src/app/maps/pages/zoom-range-page/zoom-range-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Map } from 'mapbox-gl';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-zoom-range-page',
@@ -11,15 +12,52 @@ export class ZoomRangePageComponent {
 
   @ViewChild('map') divMap?: ElementRef;
 
+  public zoom: number = 10;
+  public map?: Map;
+
+
   ngAfterViewInit(): void {
 
     if(!this.divMap) { throw new Error('Map container not found');}
 
-    const map = new Map({
+     this.map = new Map({
       container: this.divMap.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v12',
       center: [2.15, 41.5],
-      zoom: 9
+      zoom: this.zoom
     });
+
+
+    this.mapListeners();
+  }
+
+  mapListeners(){
+    if(!this.map) { throw new Error('Map not initialized');}
+
+    this.map.on('zoom', () => {
+      if(!this.map) { throw new Error('Map not initialized');}
+      this.zoom = this.map.getZoom();
+    });
+
+    this.map.on('zoomend', () => {
+      if(!this.map) { throw new Error('Map not initialized');}
+
+      if(this.map.getZoom() > 18){
+        this.map.zoomTo(18);
+      }
+    });
+  }
+
+  zoomIn() {
+    this.map?.zoomIn();
+  }
+
+  zoomOut() {
+    this.map?.zoomOut();
+  }
+
+  zoomChanged( value : string ){
+    this.zoom = Number(value);
+    this.map?.zoomTo( this.zoom );
   }
 }
